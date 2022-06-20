0e7b551be584f46ea91bba853837c84211d48e32
Code4Blessings <robincw35@gmail.com>
2022-06-20 06:29:52 -0400

    Updated 6-default-params file

--- playground/6-default-params.js
+++ playground/6-default-params.js
@@ -10,8 +10,22 @@ greeter() //Hello Person
 
 //Destructuring Default Parameters
 
-const transaction = (type, {label, stock}) => {
-    console.log(type, label, stock)
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+    rating: 4.2
 }
+// const transaction = (type, {label, stock}) => {
+//     console.log(type, label, stock)
+// }
 
-transaction('order', product)
+//transaction('order') //Will give you TypeError: Cannot destructure property `label` of 'undefined' or 'null'.
+
+//Can be destructured with a default parameter as seen below...
+
+const transaction = (type, {label, stock = 0} = {}) => {
+    console.log(type, label, stock)
+} //returns undefined or nothing at all but no type errors
+
